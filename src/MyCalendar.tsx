084d7a722e9a2ff05eb5be39e3336877fe52241b
Jasur Ramazonov <otabekturobov@gmail.com>
2025-02-26 20:27:08 +0300

    first commit

--- src/MyCalendar.tsx
+++ src/MyCalendar.tsx
@@ -7,13 +7,22 @@ const events = [{ title: "Jasurbek", date: "2025-02-26" }];
 
 const MyCalendar = () => {
   const [difference, setDifference] = useState<number>();
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
   const handleDateClick = (info: any) => {
     const today = new Date();
     const clickedDate = new Date(info.dateStr);
     const differenceInTime = clickedDate.getTime() - today.getTime();
     const differenceInDayes = Math.ceil(differenceInTime / (1000 * 3600 * 24));
     setDifference(differenceInDayes);
+    setSelectedDate(info.dateStr);
   };
+
+  function selectDay(date: any) {
+    let myDate = new Date(date.date);
+
+    return myDate.toLocaleDateString("en-CA") === selectedDate ? "my-date" : "";
+  }
+
   return (
     <div>
       <h3 className="text-center">Kunlar orasidagi farq: {difference}</h3>
@@ -23,6 +32,7 @@ const MyCalendar = () => {
         events={events}
         dateClick={handleDateClick}
         eventBackgroundColor="#27ae60"
+        dayCellClassNames={selectDay}
       />
     </div>
   );
